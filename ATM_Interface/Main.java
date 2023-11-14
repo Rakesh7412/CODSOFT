b92cf7c947e3758f11c82173cdd870f01c3fda6c
Rakesh7412 <93173731+Rakesh7412@users.noreply.github.com>
2023-11-14 14:19:04 +0530

    Update Main.java

--- ATM_Interface/Main.java
+++ ATM_Interface/Main.java
@@ -1,1 +1,18 @@
+package ATM;
 
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create a bank account with an initial balance
+        BankAccount account = new BankAccount(1000.0);
+
+        // Create and display the ATM GUI
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ATM(account).setVisible(true);
+            }
+        });
+    }
+}
